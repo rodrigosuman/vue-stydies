65f9128dd74a2788a26e8eb619f8f222d6bc02fb
RodrigoSuman <rodrigosuman18@gmail.com>
2019-12-16 15:01:19 -0300

    app view com Formulário de cadastro e lista de cadastros realizados

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
       <h1 class="display-4">{{ title }}</h1>
     </div>
 
+    <Formulario />
     <Cadastros />
   </b-container>
 </template>
 
 <script>
+import Formulario from "./components/Formulario";
 import Cadastros from "./components/Cadastros";
 
 export default {
   name: "app",
   components: {
+    Formulario,
     Cadastros
   },
   data: () => ({
